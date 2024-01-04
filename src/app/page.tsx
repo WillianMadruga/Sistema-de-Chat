ad1624d72b9ccf38df1f08fb0abca80a8d8280ff
Willian Madruga <willianmadrugadonel@gmail.com>
2024-01-04 20:43:33 -0300

    Creating initial files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { UserProvider } from "@/contexts/UserContext"
 
 export default function Home() {
   return (
-    <div className=" h-screen mx-auto  px-2 bg-black text-white">
+    <div className=" h-screen mx-auto container  px-2 bg-black text-white">
       <UserProvider >
         <ChatProvider>
           <h1 className="text-xl py-3 text-center">Chat Simples</h1>
